b41a06d400ef1df3b95ab7caf1a4328186b802e5
Падишах Павлик <awxo@mail.ru>
2022-11-30 21:11:17 +0500

    first 1commit

---
+++ bot_calculator.py
@@ -0,0 +1,13 @@
+import telebot, datetime, time, math, re
+from telebot import types
+import operations_rational as op
+import calculatortype as ty
+import operations_complex as opCom
+BOT_TOKEN = '5833580554:AAEg8Mf3Z4Qq435yzjzlnWEM2dGuGMja5zw' 
+BOT_NAME = 'calc_paul' 
+bot = telebot.TeleBot('5833580554:AAEg8Mf3Z4Qq435yzjzlnWEM2dGuGMja5zw')
+
+
+
+
+
